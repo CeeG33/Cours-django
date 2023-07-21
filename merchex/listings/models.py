a6332e7feb0de9fef2f4fb1adc2e6dc13d0d8b6d
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-07-21 10:35:16 +0200

    Premier commit

---
+++ merchex/listings/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    class Genre(models.TextChoices):
+        HIP_HOP = "HH"
+        SYNTH_POP = "SP"
+        ALTERNATIVE_ROCK = "AR"
+        BLUES = "BL"
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True)
+    
+    class Type(models.TextChoices):
+        RECORDS = "REC"
+        CLOTHING = "CL"
+        POSTERS = "PO"
+        MISCELLANEOUS = "MISC"
+
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
